6f56303b01aaa2b3d8a1374634562ab3f54c68f4
Dharay <dharay.m@gmail.com>
2020-10-29 13:33:25 +0530

    Initial Commit

---
+++ SwiftDataStructures/SwiftDataStructures.h
@@ -0,0 +1,18 @@
+//
+//  SwiftDataStructures.h
+//  SwiftDataStructures
+//
+//  Created by Mistry, Dharay Paresh on 29/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDataStructures.
+FOUNDATION_EXPORT double SwiftDataStructuresVersionNumber;
+
+//! Project version string for SwiftDataStructures.
+FOUNDATION_EXPORT const unsigned char SwiftDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDataStructures/PublicHeader.h>
+
+
